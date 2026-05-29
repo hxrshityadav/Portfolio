0180ba27976efa051356b2543c81e867662cc029
hxrshityadav <harshityadavgkp@gmail.com>
2026-05-30 00:44:37 +0530

    style: remove VS Code / Cursor Setup card from landing setup section

--- src/components/landing/Setup.tsx
+++ src/components/landing/Setup.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 
 import Container from '../common/Container';
 import SectionHeading from '../common/SectionHeading';
-import Code from '../svgs/Code';
 import Gear from '../svgs/Gear';
 import { Card } from '../ui/card';
 
@@ -14,12 +13,6 @@ const setup = [
     description: 'Productivity Tools, Gears i use to get my work done.',
     icon: <Gear className="size-4" />,
     href: '/gears',
-  },
-  {
-    name: 'VS Code / Cursor Setup',
-    description: 'VS Code / Cursor Setup i use daily.',
-    icon: <Code className="size-4" />,
-    href: '/setup',
   },
 ];
 
